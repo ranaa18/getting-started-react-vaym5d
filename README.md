eeff910bd96cd913c37064c3dde01f5300897569
ranaa18 <42075093+ranaa18@users.noreply.github.com>
2022-09-26 11:23:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# getting-started-react-vaym5d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/getting-started-react-vaym5d)
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+import { storyblokInit, apiPlugin } from '@storyblok/react';
+
+import Page from './components/Page';
+import Teaser from './components/Teaser';
+import Grid from './components/Grid';
+import Feature from './components/Feature';
+
+storyblokInit({
+  accessToken: 'vfd366cQweRBKihMFSH7rwtt',
+  use: [apiPlugin],
+  components: {
+    page: Page,
+    teaser: Teaser,
+    grid: Grid,
+    feature: Feature,
+  },
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
